2431f39d1fbd12d0a01c985a98ede1bc7ab31aea
danbi <leedanbiii@naver.com>
2023-12-04 01:02:35 +0900

    NullPointerException

---
+++ src/main/java/com/example/javabasic/ref/BigData.java
@@ -0,0 +1,6 @@
+package com.example.javabasic.ref;
+
+public class BigData {
+    Data data;
+    int count;
+}
---
+++ src/main/java/com/example/javabasic/ref/NullMain2.java
@@ -0,0 +1,9 @@
+package com.example.javabasic.ref;
+
+public class NullMain2 {
+    public static void main(String[] args) {
+        Data data = null;
+        data.value = 10; //NullPointerException 발생
+        System.out.println("data = " + data.value);
+    }
+}

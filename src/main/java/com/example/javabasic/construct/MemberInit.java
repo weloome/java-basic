ce9d8448869d6a7f6ec2e5135ab82d5c9e46d78a
danbi <leedanbiii@naver.com>
2023-12-05 16:49:44 +0900

    this

--- src/main/java/com/example/javabasic/construct/MemberInit.java
+++ src/main/java/com/example/javabasic/construct/MemberInit.java
@@ -4,4 +4,10 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    void initMember(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
 }
---
+++ src/main/java/com/example/javabasic/construct/MemberThis.java
@@ -0,0 +1,9 @@
+package com.example.javabasic.construct;
+
+public class MemberThis {
+    String nameFiled;
+
+    void initMember(String nameParameter) {
+        nameFiled = nameParameter;
+    }
+}

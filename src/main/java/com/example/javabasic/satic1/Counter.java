3078e0c46b5179d51de703805cc22aaee7ac1b7a
danbi <leedanbiii@naver.com>
2023-12-09 17:05:17 +0900

    static 변수

---
+++ src/main/java/com/example/javabasic/satic1/Counter.java
@@ -0,0 +1,5 @@
+package com.example.javabasic.satic1;
+
+public class Counter {
+    public int count;
+}
---
+++ src/main/java/com/example/javabasic/satic1/Data3.java
@@ -0,0 +1,11 @@
+package com.example.javabasic.satic1;
+
+public class Data3 {
+    public String name;
+    public static int count;
+
+    public Data3(String name) {
+        this.name = name;
+        count++;
+    }
+}

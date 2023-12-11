16fffd5002a50d15dd20bd436be1dbc2df944841
danbi <leedanbiii@naver.com>
2023-12-11 21:21:38 +0900

    final 변수와 상수 1

---
+++ src/main/java/com/example/javabasic/final1/ConstructInit.java
@@ -0,0 +1,9 @@
+package com.example.javabasic.final1;
+
+public class ConstructInit {
+    final int value;
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/example/javabasic/final1/FieldInit.java
@@ -0,0 +1,6 @@
+package com.example.javabasic.final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+}

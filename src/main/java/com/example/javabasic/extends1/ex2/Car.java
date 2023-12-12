299eca981b968923404f0cb71e0e01967ef30626
danbi <leedanbiii@naver.com>
2023-12-12 14:57:51 +0900

    상속 관계

---
+++ src/main/java/com/example/javabasic/extends1/ex2/Car.java
@@ -0,0 +1,7 @@
+package com.example.javabasic.extends1.ex2;
+
+public class Car {
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+}
---
+++ src/main/java/com/example/javabasic/extends1/ex2/ElectricCar.java
@@ -0,0 +1,8 @@
+package com.example.javabasic.extends1.ex2;
+
+public class ElectricCar extends Car {
+
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}

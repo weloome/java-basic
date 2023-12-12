4fdef1d0e5a60b7c043664b5ebff60c31b68c309
danbi <leedanbiii@naver.com>
2023-12-12 15:38:35 +0900

    상속과 접근 제어

---
+++ src/main/java/com/example/javabasic/extends1/access/ExtendsAccessMain.java
@@ -0,0 +1,10 @@
+package com.example.javabasic.extends1.access;
+
+import com.example.javabasic.extends1.access.child.Child;
+
+public class ExtendsAccessMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}

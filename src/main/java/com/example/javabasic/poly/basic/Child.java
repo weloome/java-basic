9faf0ef0a71ce283992e1db273ae1d5b38020db0
danbi <leedanbiii@naver.com>
2023-12-13 21:08:15 +0900

    다형성 시작

---
+++ src/main/java/com/example/javabasic/poly/basic/Child.java
@@ -0,0 +1,7 @@
+package com.example.javabasic.poly.basic;
+
+public class Child extends Parent {
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
---
+++ src/main/java/com/example/javabasic/poly/basic/Parent.java
@@ -0,0 +1,7 @@
+package com.example.javabasic.poly.basic;
+
+public class Parent {
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}

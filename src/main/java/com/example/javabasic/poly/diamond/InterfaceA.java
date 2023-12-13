b77734cccbac2cfe4f327b03d9164a177bfad657
danbi <leedanbiii@naver.com>
2023-12-14 02:05:46 +0900

    인터페이스 - 다중 구현

---
+++ src/main/java/com/example/javabasic/poly/diamond/InterfaceA.java
@@ -0,0 +1,6 @@
+package com.example.javabasic.poly.diamond;
+
+public interface InterfaceA {
+    void methodA();
+    void methodCommon();
+}
---
+++ src/main/java/com/example/javabasic/poly/diamond/InterfaceB.java
@@ -0,0 +1,6 @@
+package com.example.javabasic.poly.diamond;
+
+public interface InterfaceB {
+    void methodB();
+    void methodCommon();
+}
